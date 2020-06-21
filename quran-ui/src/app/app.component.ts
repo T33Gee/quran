18a3b51fc840cc4aef63e3cc4db047fd10fde1be
DESKTOP-R23SNOR\h3rb 4dventure <taariqghoord@gmail.com>
2020-06-21 18:31:44 +0200

    made api call

--- quran-ui/src/app/app.component.ts
+++ quran-ui/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'quran-ui';
+  constructor(private _http: HttpClient) { 
+    
+  }
+  async ngOnInit() {
+    console.log("hey");
+    this._http.get('http://localhost/Quran/api/index.php/welcome/sayHi').subscribe(res => {
+      console.log(res);
+    });
+  }
+  
 }
