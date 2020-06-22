813c0e9ea105d1b9101e162638101a02010a3b2e
DESKTOP-R23SNOR\h3rb 4dventure <taariqghoord@gmail.com>
2020-06-22 11:02:15 +0200

    added  bootstrap

--- quran-ui/src/app/app.module.ts
+++ quran-ui/src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {  HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LoginComponent } from './components/login/login.component';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    MatProgressSpinnerModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- quran-ui/src/app/services/backend/api-service-base.ts
+++ quran-ui/src/app/services/backend/api-service-base.ts
@@ -6,7 +6,7 @@ export class ApiService {
     public baseUrl = "http://localhost/Quran/api/index.php";
     constructor(private _http: HttpClient){
     }
-    public get(url,params): Promise<any> {
+    public get(url, params): Promise<any> {
         return this._http.get(`${this.baseUrl}/${url}`,params).toPromise();
     }
 }
